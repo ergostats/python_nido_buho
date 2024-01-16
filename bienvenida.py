d7b8e058a4914c193a4c929b829ab4d90175d203
Centro de Investigación Estadística ERGOSTATS <100483186+ergostats@users.noreply.github.com>
2024-01-16 17:55:08 -0500

    Creación del script `bienvenida.py` para aporte 1.
    Ejemplo sencillo para hacer source en la actividad de esta clase.

---
+++ bienvenida.py
@@ -0,0 +1,11 @@
+# Felicidades por completar la clase 1!
+
+def bienvenida():
+    nombre = input("Por favor, introduce tu nombre: ")
+    gatito = '''
+     /\\_/\\
+    ( o.o )
+     > ^ <
+     '''
+    print(gatito)
+    print(f"Hola {nombre}, ERGOSTATS te da la Bienvenida!")
